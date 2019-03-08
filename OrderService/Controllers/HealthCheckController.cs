be8ed7f36434e30498a099770292fd08ab8e7fc8
a-r-chandra <ravi.developer.9@gmail.com>
2019-03-08 05:00:47 -0800

    health check

---
+++ OrderService/Controllers/HealthCheckController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("health_check")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        // GET: api/HealthCheck
+        [HttpGet]
+        public OkResult Get()
+        {
+            return Ok();
+        }
+
+        
+    }
+}
